bc3916dac6ee25dde41ca72c4a120ff46f802f05
bobbyfranklin954 <bobbyfranklin954@gmail.com>
2024-11-13 14:31:07 -0500

    Added auth validation

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -47,3 +47,10 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+
+// *** Authentication *** //
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
